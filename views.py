17a3cdeff6eb9f3c686267a1be81f0d7e7042611
Brett Lockspeiser <blocks@gmail.com>
2025-05-09 16:16:56 -0700

    CORS back on

--- views.py
+++ views.py
@@ -25,7 +25,7 @@ logger = logging.getLogger("corrections-api")
 
 # Set up CORS
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
-if False and ENVIRONMENT == "production":
+if ENVIRONMENT == "production":
     CORS(
         app,
         origins=["https://unpaywall.org"],
