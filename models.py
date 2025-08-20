9b96b8ecc7f03853db57e5866bacb63f09bd5985
Brett Lockspeiser <blocks@gmail.com>
2025-08-20 10:47:23 -0400

    Add create_new column

--- models.py
+++ models.py
@@ -3,11 +3,12 @@ from app import db
 class Curation(db.Model):
   __tablename__ = 'curations'
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  status = db.Column(db.Text) # "needs-moderation", "approved", "denied", "live"
+  status = db.Column(db.Text) # "needs-moderation", "approved", "denied"
   entity = db.Column(db.Text)
   entity_id = db.Column(db.Text)
   property = db.Column(db.Text)
   property_value = db.Column(db.Text)
+  create_new = db.Column(db.Boolean, nullable=False, default=False)
   submitter_email = db.Column(db.Text)
   moderator_email = db.Column(db.Text)
   submitted_date = db.Column(db.DateTime)
@@ -23,6 +24,7 @@ class Curation(db.Model):
       "entity_id": self.entity_id,
       "property": self.property,
       "property_value": self.property_value,
+      "create_new": self.create_new,
       "submitter_email": self.submitter_email,
       "moderator_email": self.moderator_email,
       "submitted_date": self.submitted_date,
